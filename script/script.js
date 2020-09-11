be7431714ff7b1ec91480046a6f625068b8225f8
Saurav <sauravptl@.github.com>
2020-09-11 21:20:01 +0530

    9/11

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
+// responsive topbar
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
 
+navBarToggle.addEventListener("click", function () {
+  mainNav.classList.toggle("active");
+});
